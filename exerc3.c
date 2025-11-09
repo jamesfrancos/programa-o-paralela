a35b0a700e976a21b66ed0bfec428768d4684f01
jamesfrancos <137724932+jamesfrancos@users.noreply.github.com>
2025-11-09 12:19:18 -0300

    Update exerc3.c

--- exerc3.c
+++ exerc3.c
@@ -11,16 +11,17 @@ void iterarVetor(double valor,double *vetor,int tam){
 
 int main(int argc,char **argv){
 
+    if(argc != 3){
+        printf("falta de argumentos para a execucao\n");
+
+        exit(1);
+    }
+
     int size,rank;
     int tag = 100;
     int tam = atoi(argv[1]);
     int trocas = atoi(argv[2]);
     double times[2];
-
-    if(argc != 3){
-        printf("quantidade de argumentos invalida");
-        return 1;
-    }
   
     MPI_Init(&argc,&argv);
 
@@ -35,63 +36,38 @@ int main(int argc,char **argv){
 
         exit(1);
     }else{
+        MPI_Status status;
+        double *vetor;
+        vetor = (double *)malloc(tam*sizeof(double));
+        iterarVetor(-1.0,vetor,tam);
+
         if(rank == 0){
-            double *vetor_procs1;
-            vetor_procs1 = (double *)malloc(tam*sizeof(double));
-            for(int i = 0;i<tam;i++){
-                vetor_procs1[i] = -1.0;
+            iterarVetor(2.0,vetor,tam);
+
+            double inicio = MPI_Wtime();
+
+            for(int i=0;i<trocas;i++){
+                MPI_Send(vetor,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD);
+                MPI_Recv(vetor,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD,&status);
             }
-         
-            MPI_Status status;
 
-            iterarVetor(2.0,vetor_procs1,tam);
+            double fim = MPI_Wtime();
 
-            MPI_Send(vetor_procs1,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD);   
-          
-           // for(int i=0;i<trocas;i++){
-           //     MPI_Recv(vetor_procs1,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD,&status);
-           //     iterarVetor(2.0,vetor_procs1,tam);
-           //     MPI_Send(vetor_procs1,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD);
-           // }
+            double tempo_total = fim - inicio;
 
-            free(vetor_procs1);
+            printf("tempo total: %f segundos\n",tempo_total);
 
         }else if(rank == 1){
-            double *vetor_procs2;
-            MPI_Status status;
-            vetor_procs2 = (double*)malloc(tam*sizeof(double));
-            for(int i = 0;i<tam;i++){
-                vetor_procs2[i] = -1.0;
+            for(int i=0;i<trocas;i++){
+                MPI_Recv(vetor,tam,MPI_DOUBLE,0,tag,MPI_COMM_WORLD,&status);
+                iterarVetor(4.0,vetor,tam);
+                MPI_Send(vetor,tam,MPI_DOUBLE,0,tag,MPI_COMM_WORLD);
             }
-  
-            MPI_Recv(vetor_procs2,tam,MPI_DOUBLE,0,tag,MPI_COMM_WORLD,&status);
-
-            iterarVetor(4.0,vetor_procs2,tam);
- 
-           // MPI_Send(vetor_procs2,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD);
-            
-            //for(int i=0;i<trocas;i++){
-                //MPI_Recv(vetor_procs2,tam,MPI_DOUBLE,0,tag,MPI_COMM_WORLD,&status);
-               // iterarVetor(4.0,vetor_procs2,tam);
-              //  if(i < trocas-1){
-             //      MPI_Send(vetor_procs2,tam,MPI_DOUBLE,1,tag,MPI_COMM_WORLD);
-            //    }
-           // }
-          //  free(vetor_procs2);
         }
+
+        free(vetor);
     }
-
-    double end_time = MPI_Wtime();
-
-    double time = end_time - start_time;
-
-    times[rank] = time;
-
     MPI_Finalize();
-
-   if(rank == 1){
-      printf("tempo total : %f segundos\n",times[0]+times[1]);
-   }
 
     return 0;
 }
